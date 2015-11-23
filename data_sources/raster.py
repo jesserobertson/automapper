8e9688b7f5a73206f0864cb922d7c4173615169e
Jess Robertson <jesse.robertson@csiro.au>
2015-11-23 03:09:55 +0000

    Initial commit

---
+++ data_sources/raster.py
@@ -0,0 +1,15 @@
+""" file: raster.py
+
+    Imports for raster data
+"""
+
+nspace = {
+    "ows": "http://www.opengis.net/ows",
+    "ogc": "http://www.opengis.net/ogc",
+    "wfs": "http://www.opengis.net/wfs",
+    "gml": "http://www.opengis.net/gml",
+    "gsml": "urn:cgi:xmlns:CGI:GeoSciML:2.0",
+    "sa": "http://www.opengis.net/sampling/1.0",
+    "xlink": "http://www.w3.org/1999/xlink",
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance"
+}
